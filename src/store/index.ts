82a051e3529dca2f7b19b2ffff2853bbc73ac4bc
Jesus Zuñiga <jesuszt@hotmail.com>
2023-03-22 11:33:25 -0600

    Creación del módulo places

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,21 +1,17 @@
 import { createStore } from 'vuex';
 
 // My custom modules
-//import exampleModule from './module-template';
-//import { ExampleStateInterface } from './module-template/state';
+import placesModule from './places';
+import { PlacesState } from './places/state';
 
 
 export interface StateInterface {
-  // Define your own store structure, using submodules if needed
-  // example: ExampleStateInterface;
-  // Declared as unknown to avoid linting issue. Best to strongly type as per the line above.
-  //example: ExampleStateInterface
-  example: unknown
+  places: PlacesState,
 }
 
 export default createStore<StateInterface>({
   modules: {
-    //example: exampleModule
-    example: {}
+    places: placesModule
   }
-})
+});
+
