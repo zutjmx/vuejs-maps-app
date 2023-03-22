63a44234ae9fed097ec7cd3531d3b459514ff6a5
Jesus Zuñiga <jesuszt@hotmail.com>
2023-03-22 11:10:39 -0600

    Configuración del store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,21 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+// My custom modules
+//import exampleModule from './module-template';
+//import { ExampleStateInterface } from './module-template/state';
+
+
+export interface StateInterface {
+  // Define your own store structure, using submodules if needed
+  // example: ExampleStateInterface;
+  // Declared as unknown to avoid linting issue. Best to strongly type as per the line above.
+  //example: ExampleStateInterface
+  example: unknown
+}
+
+export default createStore<StateInterface>({
   modules: {
+    //example: exampleModule
+    example: {}
   }
 })
